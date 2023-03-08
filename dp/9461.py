1649220bf958c170381f334ce1693232ca0281b8
YooJaeHyeon <jhyoo1203@gmail.com>
2023-03-08 19:08:10 +0900

    success

---
+++ dp/9461.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+dp = [0 for i in range(101)]
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+for i in range(4, 101):
+    dp[i] = dp[i-3]+dp[i-2]
+
+for _ in range(T):
+    n = int(input())
+    print(dp[n])
