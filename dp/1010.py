1e7414c1369e41d78339571fa79361dd07293f60
YooJaeHyeon <jhyoo1203@gmail.com>
2023-12-24 17:32:23 +0900

    Success

---
+++ dp/1010.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    dp = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
+
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            if i == 1:
+                dp[i][j] = j
+                continue
+            if i == j:
+                dp[i][j] = 1
+            else:
+                if j > i:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1]
+
+    print(dp[N][M])
