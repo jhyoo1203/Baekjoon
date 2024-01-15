ea8adfe5e2c772384c46ad2503f729c5f6ea5dca
YooJaeHyeon <jhyoo1203@gmail.com>
2024-01-15 21:07:05 +0900

    Success

---
+++ dp/16194.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+p = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(N + 1)]
+
+
+for i in range(1, N + 1):
+    for k in range(1, i + 1):
+        if not dp[i]:
+            dp[i] = dp[i - k] + p[k]
+        else:
+            dp[i] = min(dp[i], dp[i - k] + p[k])
+
+print(dp[N])
