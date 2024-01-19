96b20ad26de8f38d4bb8411034281700ff4de956
YooJaeHyeon <jhyoo1203@gmail.com>
2024-01-19 21:48:45 +0900

    Success

---
+++ solved_ac_class/1927.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, x)
