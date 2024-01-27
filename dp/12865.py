712c7ec2506a4aef09bed9e49e2ddbee4414b0fc
YooJaeHyeon <jhyoo1203@gmail.com>
2024-01-27 22:31:47 +0900

    Success

---
+++ dp/12865.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+items = []
+for _ in range(N):
+    W, V = map(int, input().split())
+    items.append((W, V))
+dp = [0] * (K + 1)
+
+for item in items:
+    W, V = item
+    for i in range(K, W - 1, -1):
+        dp[i] = max(dp[i], dp[i - W] + V)
+
+print(max(dp))
