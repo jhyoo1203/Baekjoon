60ec181059606cde61eba800faae902b91f48e1f
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-02-02 18:22:16 +0900

    Success

---
+++ greedy/1343.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+board = input().rstrip()
+
+board = board.replace("XXXX", "AAAA")
+board = board.replace("XX", "BB")
+
+if 'X' in board:
+    print(-1)
+    
+else:
+    print(board)
