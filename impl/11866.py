d8e8c602556255be03dd000463b74231bae50798
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-03-13 23:44:18 +0900

    Success

---
+++ impl/11866.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+
+queue = deque()
+result = []
+
+n, k = map(int, sys.stdin.readline().split());
+
+for i in range(1, n+1):
+    queue.append(i)
+
+while queue:
+    for i in range(k-1):
+        queue.append(queue.popleft())
+    result.append(queue.popleft())
+
+print("<", end='')
+print(', '.join(map(str,result)), end='')
+print(">")
